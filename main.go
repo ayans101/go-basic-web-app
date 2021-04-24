b18d81fa84b276736f7e359e07cb14a1f294199a
Ayan Sarkar <ayansarkar367@gmail.com>
2021-04-25 00:07:26 +0530

    add handler functions

--- main.go
+++ main.go
@@ -5,16 +5,29 @@ import (
 	"net/http"
 )
 
+const portNumber = ":4000"
+
+//	Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+//	About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+func addValues(x, y int) int {
+	return x + y
+}
+
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello World!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println("Number of bytes written:", n)
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-	http.ListenAndServe(":4000", nil)
-	
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	http.ListenAndServe(portNumber, nil)
+
 }
