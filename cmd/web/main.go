affa7655a0881c67799f7ecfe471401b2a164491
Ayan Sarkar <ayansarkar367@gmail.com>
2021-04-25 20:33:32 +0530

    set application wide configuration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/ayans101/go-basic-web-app/pkg/config"
 	"github.com/ayans101/go-basic-web-app/pkg/handlers"
+	"github.com/ayans101/go-basic-web-app/pkg/render"
 )
 
 const portNumber = ":4000"
 
 func main() {
+	var app config.AppConfig
 
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
