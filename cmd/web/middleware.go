90318448466c9e14ca05c61297428a20211a4b11
Ayan Sarkar <ayansarkar367@gmail.com>
2021-04-27 17:49:54 +0530

    add middleware to put CSRF prevention tokens in cookies

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/justinas/nosurf"
 )
 
-func WriteToConsole(next http.Handler) http.Handler {
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
 
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Hit the page")
-		next.ServeHTTP(w, r)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   false,
+		SameSite: http.SameSiteLaxMode,
 	})
 
+	return csrfHandler
 }
